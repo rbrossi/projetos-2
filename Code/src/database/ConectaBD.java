a58175aff137396bcd2d1063c27d1408832fde27
Douglas Portugal de Oliveira <douglas-portuga@hotmail.com>
2018-06-16 14:21:11 -0300

    troca para nova DB

--- Code/src/database/ConectaBD.java
+++ Code/src/database/ConectaBD.java
@@ -19,8 +19,8 @@ public class ConectaBD {
 		
 		try {			
 			Class.forName("com.mysql.jdbc.Driver");
-/*Base antiga 1 table*/		return  DriverManager.getConnection("jdbc:mysql://mysqlportuga.unifique.com.br/mysqlpor_projetos2_db", "mysqlpor_projeto", "mysqllimpai");
-// Nova base				return  DriverManager.getConnection("jdbc:mysql://mysqlportuga.unifique.com.br/mysqlpor_limpai_db", "mysqlpor_limpai", "mysqllimpai");
+//Base antiga 1 table		return  DriverManager.getConnection("jdbc:mysql://mysqlportuga.unifique.com.br/mysqlpor_projetos2_db", "mysqlpor_projeto", "mysqllimpai");
+/* Nova base*/				return  DriverManager.getConnection("jdbc:mysql://mysqlportuga.unifique.com.br/mysqlpor_limpai_db", "mysqlpor_limpai", "mysqllimpai");
 //	Base local				return  DriverManager.getConnection("jdbc:mysql://127.0.0.1/limpai", "root", "root");			
 	} catch (Exception e) {
 		throw new Exception(e);
